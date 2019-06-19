d1d0488a31d2b9297f391f939a1587a3298a39e8
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-06-19 16:25:14 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(numba)
+while numba > 0
+  puts "#{numba} SECOND(S)!\n"
+  numba -= 1
+  sleep 1
 end
+return "HAPPY NEW YEAR!"
+end
+
+countdown(10)
